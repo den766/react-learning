c0ad30eeb5396aa3af6cd41e656eef49de8aae83
den766 <asharafalika886@gmail.com>
2026-05-13 13:38:34 +0530

    debug: trace fetch lifecycle with console logs

--- src/experiments/week3/day4-fetchingData/debugging.jsx
+++ src/experiments/week3/day4-fetchingData/debugging.jsx
@@ -3,21 +3,31 @@ import { useState, useEffect } from "react";
 function FetchUsers() {
   const [users, setUsers] = useState([]);
 
+  console.log(users);
+
   console.log("render Phase");
+
+  
 
   useEffect(() => {
     console.log("effect run started");
 
     async function getUser() {
+        console.log("fetch Started");
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/users",
       );
 
+      console.log("response Arrived");  
+
       const data = await response.json();
+
+      console.log("Json Parsed");
 
       console.log(data);
 
       setUsers(data);
+      console.log("state update requested");
     }
 
     getUser();
