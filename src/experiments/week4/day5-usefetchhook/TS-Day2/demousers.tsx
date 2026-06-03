2806b876e49fbd558c8d55be1882f079fc9bf1c4
den766 <asharafalika886@gmail.com>
2026-06-03 13:33:59 +0530

    practice typescript object arrays types and optional chaining

--- src/experiments/week4/day5-usefetchhook/TS-Day2/demousers.tsx
+++ src/experiments/week4/day5-usefetchhook/TS-Day2/demousers.tsx
@@ -1,19 +1,74 @@
-
-
 function DemoUsers() {
+  // const users  : string[]= ["Jhone" , "Walt" , "Angela" , ];
 
-     const users  : string[]= ["Jhone" , "Walt" , "Angela" , ];
+  type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description?: string;
+  };
 
-     return (
-          <>
-          <h1>Demo Users</h1>
-          {users.map((user)=> (
+  const products: Product[] = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 50000,
+    },
+    {
+      id: 2,
+      name: "Mobile",
+      price: 20000,
+    },
+  ];
 
-              <p key={user}>{user.toUpperCase()}</p>
-          ))}
-          </>
-          
-     )
+//   const product1: Product = { id: 1, name: "well", price: 3000 };
+
+//   const products2: Product[] = [{ id: 1, name: "well", price: 3000 }];
+
+//   console.log(product1);
+
+const featuredProducts : Product[] = [
+    {
+      id: 1,
+      name: "Macbook pro",
+      price: 50000,
+    },
+    {
+      id: 2,
+      name: "Iphone",
+      price: 20000,
+    },
+]
+
+const cart : Product[] = [ 
+
+      {
+      id: 1,
+      name: "Laptop",
+      price: 50000,
+      description: "Awesome"
+    },
+    {
+      id: 2,
+      name: "Mobile",
+      price: 20000,
+      description : "Excellent"
+    },
+]
+
+// const Cap = cart[0].description?.toUpperCase();
+// console.log(Cap);
+  return (
+    <>
+      <h1>Demo Users</h1>
+      {cart.map((user) => (
+        <div>
+          <p key={user.id}>{user.description?.toUpperCase()}</p>
+          {/* <p key={user.id}>{user.catogery}</p> */}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default DemoUsers;
